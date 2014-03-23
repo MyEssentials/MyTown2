b814af73ea9d9cd17e4e8dbccb6a2661a2b859fb
Joe Goett <jgoett154@gmail.com>
2014-03-22 23:23:01 -0400

    Fixed stuff? Idk anymore...

--- MyTown/src/main/java/mytown/MyTown.java
+++ MyTown/src/main/java/mytown/MyTown.java
@@ -3,31 +3,70 @@ package mytown;
 import java.io.File;
 
 import mytown.core.Log;
+import mytown.datasource.MyTownDatasource;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.FMLServerStoppingEvent;
 
-@Mod(modid = Constants.MODID, name=Constants.MODNAME, version=Constants.VERSION, dependencies=Constants.DEPENDENCIES)
+@Mod(modid = Constants.MODID, name = Constants.MODNAME, version = Constants.VERSION, dependencies = Constants.DEPENDENCIES)
 public class MyTown {
 	@Mod.Instance
 	public static MyTown INSTANCE;
-	
+
 	// Loggers
-	Log coreLog;
-	Log dbLog;
-	Log bypassLog;
-	
+	public Log coreLog;
+	public Log bypassLog;
+	public Log datasourceLog;
+
 	// Configs
-	Config config;
-	
+	public Config config;
+
+	// Datasource
+	public MyTownDatasource datasource;
+
 	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent ev){
+	public void preInit(FMLPreInitializationEvent ev) {
 		// Setup Loggers
-		coreLog = new Log("MyTown", FMLLog.getLogger());
-		dbLog = new Log("MyTownDB", coreLog.getLogger());
-		bypassLog = new Log("MyTownBypass", coreLog.getLogger());
-		
+		coreLog = new Log("MyTown2", FMLLog.getLogger());
+		datasourceLog = new Log("Datasource", coreLog.getLogger());
+		bypassLog = new Log("Bypass", coreLog.getLogger());
+
+		Constants.CONFIG_FOLDER = ev.getModConfigurationDirectory() + "/MyTown/";
+
 		// Read Configs
-		config = new Config(new File(ev.getModConfigurationDirectory(), "MyTown/MyTown.cfg"));
+		config = new Config(new File(Constants.CONFIG_FOLDER, "MyTown.cfg"));
+		config.load();
+	}
+
+	@Mod.EventHandler
+	public void serverStarting(FMLServerStartingEvent ev) {
+		try { // Try to load the Datasource
+			datasource = (MyTownDatasource) Class.forName("mytown.datasource.impl.MyTownDatasource_" + config.dbType.toLowerCase()).newInstance();
+			datasource.configure(config, datasourceLog);
+			if (config.hasChanged())
+				config.save();
+			if (!datasource.connect()) {
+				datasourceLog.severe("Failed to connect to datasource!");
+			}
+		} catch (ClassNotFoundException ex) {
+			datasourceLog.severe("Failed to find datasource type: %s", config.dbType);
+		} catch (InstantiationException e) {
+			datasourceLog.severe("Failed to create datasource. %s", e.getLocalizedMessage());
+		} catch (Exception e) {
+			e.printStackTrace(); // TODO Maybe change?
+		}
+	}
+
+	@Mod.EventHandler
+	public void serverStopping(FMLServerStoppingEvent ev) {
+		try {
+			datasource.save();
+			datasource.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace(); // TODO Maybe change?
+		}
+		config.save();
 	}
 }
