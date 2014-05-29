65def8dd019740fcc7bb5ad3636eace7d883dcf1
AfterWind <AfterWind@gmail.com>
2014-05-29 19:42:40 +0300

    Added missing file

---
+++ MyTown/src/main/java/mytown/entities/town/AdminTown.java
@@ -0,0 +1,25 @@
+package mytown.entities.town;
+
+import mytown.entities.Rank;
+import mytown.entities.Resident;
+import mytown.entities.TownBlock;
+
+
+public class AdminTown extends Town {
+
+	public AdminTown(String name) {
+		super(name, 0);
+	}
+	
+	@Override
+	public boolean addTownBlock(TownBlock block) {
+		return townBlocks.add(block);
+		// TODO: To be edited when checking distance between towns.
+	}
+	
+	@Override
+	public void addResident(Resident res, Rank rank) {
+		// Nothing, since admin towns shouldn't have residents
+	}
+	
+}
