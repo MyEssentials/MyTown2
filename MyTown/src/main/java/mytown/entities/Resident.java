6e2d2820f497acdfa5ae060436808bb93ccd0243
Joe Goett <jgoett154@gmail.com>
2014-06-27 00:32:34 -0400

    Added one last note about an NPE. It occures when first joining it looks like. Im too tired to do anything else though tonight.

--- MyTown/src/main/java/mytown/entities/Resident.java
+++ MyTown/src/main/java/mytown/entities/Resident.java
@@ -197,7 +197,7 @@ public class Resident implements IPlotSelector {
 			TownBlock oldTownBlock, newTownBlock;
 
 			oldTownBlock = MyTown.getDatasource().getTownBlock(lastDim, oldChunkX, oldChunkZ, true);
-			newTownBlock = MyTown.getDatasource().getTownBlock(player.dimension, newChunkX, newChunkZ, true);
+			newTownBlock = MyTown.getDatasource().getTownBlock(player.dimension, newChunkX, newChunkZ, true); // TODO This throws an NPE. Need to fix
 
 			if (oldTownBlock == null && newTownBlock != null || oldTownBlock != null && newTownBlock != null && !oldTownBlock.getTown().getName().equals(newTownBlock.getTown().getName())) {
 				if (isPartOfTown(newTownBlock.getTown())) {
