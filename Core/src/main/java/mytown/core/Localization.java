1eae325008a40c58845932d82f3094c2a44c5b08
Joe Goett <jgoett154@gmail.com>
2014-06-26 21:14:52 -0400

    Changed how Localizations can be loaded. Now supports loading directly from the classpath!

--- Core/src/main/java/mytown/core/Localization.java
+++ Core/src/main/java/mytown/core/Localization.java
@@ -2,8 +2,10 @@ package mytown.core;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,31 +16,37 @@ import java.util.Map;
  */
 public class Localization {
 	/**
-	 * The localization file
-	 */
-	File file;
-
-	/**
 	 * The localization map
 	 */
 	Map<String, String> localizations;
+	Reader reader = null;
+	
+	/**
+	 * Specifies the {@link Reader} to use when reading the localization
+	 * @param r
+	 */
+	public Localization(Reader r) {
+		this.reader = r;
+		localizations = new HashMap<String, String>();
+	}
 
 	/**
-	 * Specifies the file to load via a File
+	 * Specifies the file to load via a {@link File}
 	 * 
 	 * @param file
+	 * @throws FileNotFoundException 
 	 */
-	public Localization(File file) {
-		this.file = file;
-		localizations = new HashMap<String, String>();
+	public Localization(File file) throws FileNotFoundException {
+		this(new FileReader(file));
 	}
 
 	/**
 	 * Specifies the file to load via a filename
 	 * 
 	 * @param filename
+	 * @throws FileNotFoundException 
 	 */
-	public Localization(String filename) {
+	public Localization(String filename) throws FileNotFoundException {
 		this(new File(filename));
 	}
 
@@ -48,8 +56,7 @@ public class Localization {
 	 * @throws IOException
 	 */
 	public void load() throws IOException {
-		FileReader fr = new FileReader(file);
-		BufferedReader br = new BufferedReader(fr);
+		BufferedReader br = new BufferedReader(reader);
 
 		String line;
 		while ((line = br.readLine()) != null) {
--- MyTown/src/main/java/mytown/proxies/LocalizationProxy.java
+++ MyTown/src/main/java/mytown/proxies/LocalizationProxy.java
@@ -1,12 +1,8 @@
 package mytown.proxies;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
 
-import mytown.Constants;
 import mytown.MyTown;
 import mytown.config.Config;
 import mytown.core.Localization;
@@ -16,38 +12,18 @@ public class LocalizationProxy {
 	private static Localization localization;
 	private static Log log = MyTown.instance.log.createChild("Localization");
 
+	/**
+	 * Loads the {@link Localization} from the classpath.
+	 * 
+	 * Its loaded as an {@link InputStream}, then turned into an {@link InputStreamReader} and passed to {@link Localization}
+	 */
 	public static void load() {
-		File localFile = new File(Constants.CONFIG_FOLDER, "localization/" + Config.localization + ".lang");
-		if (!localFile.getParentFile().exists()) {
-			localFile.getParentFile().mkdir();
-		}
-		if (!localFile.exists()) {
-			InputStream is = MyTown.class.getResourceAsStream("/localization/" + Config.localization + ".lang");
-			if (is != null) {
-				OutputStream resStreamOut = null;
-				int readBytes;
-				byte[] buffer = new byte[4096];
-				try {
-					resStreamOut = new FileOutputStream(localFile);
-					while ((readBytes = is.read(buffer)) > 0) {
-						resStreamOut.write(buffer, 0, readBytes);
-					}
-				} catch (IOException e1) {
-					// TODO Handle this
-				} finally {
-					try {
-						is.close();
-						resStreamOut.close();
-					} catch (Exception ignored) {
-					}
-				}
-			}
-		}
 		try {
-			LocalizationProxy.localization = new Localization(new File(Constants.CONFIG_FOLDER, "localization/" + Config.localization + ".lang"));
+			InputStream is = MyTown.class.getResourceAsStream("/localization/" + Config.localization + ".lang");
+			LocalizationProxy.localization = new Localization(new InputStreamReader(is));
 			LocalizationProxy.localization.load();
-		} catch (Exception e) {
-			LocalizationProxy.log.warning("Localization file %s missing!", Config.localization);
+		} catch(Exception ex) {
+			LocalizationProxy.log.warning("Failed to load localization file (%s)!", ex, Config.localization);
 		}
 	}
 
