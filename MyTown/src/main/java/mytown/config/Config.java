d434df2432b4a92bfbf8fe13217e8047bb7d7dba
Joe Goett <jgoett154@gmail.com>
2014-07-23 18:09:47 -0400

    Forgot /n will write a newline. Changed it to //n so forge will load/save correctly.

--- MyTown/src/main/java/mytown/config/Config.java
+++ MyTown/src/main/java/mytown/config/Config.java
@@ -6,7 +6,7 @@ import mytown.core.utils.config.ConfigProperty;
 public class Config {
 	/* ----- General Config ----- */
 
-	@ConfigProperty(category = "general", name = "Localization", comment = "Localization file without file extension.\nLoaded from config/MyTown/localization/ first, then from the jar, then finally will fallback to en_US if needed.")
+	@ConfigProperty(category = "general", name = "Localization", comment = "Localization file without file extension.\\nLoaded from config/MyTown/localization/ first, then from the jar, then finally will fallback to en_US if needed.")
 	public static String localization = "en_US";
 
 	@ConfigProperty(category = "general", name = "SafeModeMessage", comment = "Message to display to users when MyTown is in safemode.")
@@ -21,20 +21,20 @@ public class Config {
     // TODO Finish default formats (Colors, more fields, etc)
 
     @ConfigProperty(category = "formatter", name = "BlockInfoFormat", comment = "The format to display a Block's info.")
-    public static String blockInfoFormat = " ---------- Block ----------\nTown: %1$s\nDimension: %2$s\nLocation: %3$s";
+    public static String blockInfoFormat = " ---------- Block ----------\\nTown: %1$s\\nDimension: %2$s\\nLocation: %3$s";
 
     @ConfigProperty(category = "formatter", name = "NationInfoFormat", comment = "The format to display a Nation's info.")
-    public static String nationInfoFormat = " ---------- %1$s  ----------\nCapital: TODO";
+    public static String nationInfoFormat = " ---------- %1$s  ----------\\nCapital: TODO";
 
     @ConfigProperty(category = "formatter", name = "PlotInfoFormat", comment = "The format to display a Plots's info.")
-    public static String plotInfoFormat = " ---------- %1$s  ----------\nTown: %2$s\nDimension: %3$s\nStart: %4$s\nEnd: %5$s";
+    public static String plotInfoFormat = " ---------- %1$s  ----------\\nTown: %2$s\\nDimension: %3$s\\nStart: %4$s\\nEnd: %5$s";
 
     @ConfigProperty(category = "formatter", name = "RankInfoFormat", comment = "The format to display a Rank's info.")
-    public static String rankInfoFormat = " ---------- %1$s  ----------\nPermissions: %2$s";
+    public static String rankInfoFormat = " ---------- %1$s  ----------\\nPermissions: %2$s";
 
     @ConfigProperty(category = "formatter", name = "ResidentInfoFormat", comment = "The format to display a Resident's info.")
     public static String residentInfoFormat = " ---------- %1$s  ----------";
 
     @ConfigProperty(category = "formatter", name = "TownInfoFormat", comment = "The format to display a Town's info.")
-    public static String townInfoFormat = " ---------- %1$s (R: %2$s | B: %3$s | P: %4$s) ----------\nResidents: %5$s\nRanks %6$s";
+    public static String townInfoFormat = " ---------- %1$s (R: %2$s | B: %3$s | P: %4$s) ----------\\nResidents: %5$s\\nRanks %6$s";
 }
