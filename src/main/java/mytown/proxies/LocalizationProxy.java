f7eaa4da0a1a1b161b87fc472fbb35d3046d09ba
Joe Goett <jgoett154@gmail.com>
2014-11-11 15:28:57 -0500

    Slowly moving away from custom log to log4j2 Logger.

--- src/main/java/mytown/proxies/LocalizationProxy.java
+++ src/main/java/mytown/proxies/LocalizationProxy.java
@@ -1,10 +1,10 @@
 package mytown.proxies;
 
-import mytown.MyTown;
 import mytown.config.Config;
 import mytown.core.Localization;
-import mytown.core.utils.Log;
 import mytown.util.Constants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,8 +12,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class LocalizationProxy {
+    private static Logger logger = LogManager.getLogger("MyTown2.Localization");
     private static Localization localization;
-    private static Log log = MyTown.instance.log.createChild("Localization");
 
     /**
      * Loads the {@link Localization}. First tries to load it from config/MyTown/Localization, then the classpath, then loads en_US in its place
@@ -27,17 +27,17 @@ public class LocalizationProxy {
                 is = new FileInputStream(file);
             }
             if (is == null) {
-                is = MyTown.class.getResourceAsStream("/localization/" + Config.localization + ".lang");
+                is = LocalizationProxy.class.getResourceAsStream("/localization/" + Config.localization + ".lang");
             }
             if (is == null) {
-                is = MyTown.class.getResourceAsStream("/localization/en_US.lang");
-                log.warn("Reverting to en_US.lang because %s does not exist!", Config.localization + ".lang");
+                is = LocalizationProxy.class.getResourceAsStream("/localization/en_US.lang");
+                logger.warn("Reverting to en_US.lang because {} does not exist!", Config.localization + ".lang");
             }
 
             LocalizationProxy.localization = new Localization(new InputStreamReader(is));
             LocalizationProxy.localization.load();
         } catch (Exception ex) {
-            LocalizationProxy.log.warn("Failed to load localization file (%s)!", ex, Config.localization);
+            logger.warn("Failed to load localization!", ex);
         }
     }
 
