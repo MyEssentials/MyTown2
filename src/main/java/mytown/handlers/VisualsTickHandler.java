b33b8e04487d0d63c46c08655739b18d02712dd0
AfterWind <afterwinddesu@gmail.com>
2015-03-16 18:35:41 +0200

    Fixed issue with showing blocks in VisualsTickHandler

--- src/main/java/mytown/handlers/VisualsTickHandler.java
+++ src/main/java/mytown/handlers/VisualsTickHandler.java
@@ -53,8 +53,6 @@ public class VisualsTickHandler {
                         iterator.remove();
                     }
                 }
-                //if(set.getValue().size() == 0)
-                //    markedBlocks.remove(set.getKey());
             }
         }
     }
