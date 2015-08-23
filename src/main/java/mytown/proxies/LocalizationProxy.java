a5ba41ed654feefe8d7d811978cc6c531557599d
AfterWind <afterwinddesu@gmail.com>
2015-08-23 21:13:26 +0300

    Fixed localization issue.

--- src/main/java/mytown/proxies/LocalizationProxy.java
+++ src/main/java/mytown/proxies/LocalizationProxy.java
@@ -19,25 +19,27 @@ public class LocalizationProxy {
 
     public static Localization getLocalization() {
 
-        try {
-            InputStream is = null;
+        if(localization == null) {
+            try {
+                InputStream is = null;
 
-            File file = new File(Constants.CONFIG_FOLDER + "/localization/" + Config.localization + ".lang");
-            if (file.exists()) {
-                is = new FileInputStream(file);
-            }
-            if (is == null) {
-                is = LocalizationProxy.class.getResourceAsStream("/localization/" + Config.localization + ".lang");
-            }
-            if (is == null) {
-                is = LocalizationProxy.class.getResourceAsStream("/localization/en_US.lang");
-                MyTown.instance.LOG.warn("Reverting to en_US.lang because {} does not exist!", Config.localization + ".lang");
-            }
+                File file = new File(Constants.CONFIG_FOLDER + "/localization/" + Config.localization + ".lang");
+                if (file.exists()) {
+                    is = new FileInputStream(file);
+                }
+                if (is == null) {
+                    is = LocalizationProxy.class.getResourceAsStream("/mytown/localization/" + Config.localization + ".lang");
+                }
+                if (is == null) {
+                    is = LocalizationProxy.class.getResourceAsStream("/mytown/localization/en_US.lang");
+                    MyTown.instance.LOG.warn("Reverting to en_US.lang because {} does not exist!", Config.localization + ".lang");
+                }
 
-            localization = new Localization(new InputStreamReader(is));
-            localization.load();
-        } catch (Exception ex) {
-            MyTown.instance.LOG.warn("Failed to load localization!", ex);
+                localization = new Localization(new InputStreamReader(is));
+                localization.load();
+            } catch (Exception ex) {
+                MyTown.instance.LOG.warn("Failed to load localization!", ex);
+            }
         }
         return localization;
     }
