9f75706041f244a9788b342eb2c68a76a8a08fed
AfterWind <afterwinddesu@gmail.com>
2015-08-23 16:01:25 +0300

    Fixed #293

--- src/main/java/mytown/proxies/LocalizationProxy.java
+++ src/main/java/mytown/proxies/LocalizationProxy.java
@@ -1,8 +1,13 @@
 package mytown.proxies;
 
 import myessentials.Localization;
+import mytown.MyTown;
+import mytown.config.Config;
 import mytown.util.Constants;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class LocalizationProxy {
@@ -13,9 +18,26 @@ public class LocalizationProxy {
     }
 
     public static Localization getLocalization() {
-        if(localization == null) {
-            localization = new Localization(new InputStreamReader(LocalizationProxy.class.getResourceAsStream("/localization/en_US.lang")));
+
+        try {
+            InputStream is = null;
+
+            File file = new File(Constants.CONFIG_FOLDER + "/localization/" + Config.localization + ".lang");
+            if (file.exists()) {
+                is = new FileInputStream(file);
+            }
+            if (is == null) {
+                is = LocalizationProxy.class.getResourceAsStream("/localization/" + Config.localization + ".lang");
+            }
+            if (is == null) {
+                is = LocalizationProxy.class.getResourceAsStream("/localization/en_US.lang");
+                MyTown.instance.LOG.warn("Reverting to en_US.lang because {} does not exist!", Config.localization + ".lang");
+            }
+
+            localization = new Localization(new InputStreamReader(is));
             localization.load();
+        } catch (Exception ex) {
+            MyTown.instance.LOG.warn("Failed to load localization!", ex);
         }
         return localization;
     }
