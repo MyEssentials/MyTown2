1f7a3470bec8791181af219c1589c6cb7c6d3ee0
krwminer <krwshop@hotmail.com>
2015-10-29 10:12:34 -0400

    Force new build.

--- src/main/java/mytown/protection/ProtectionManager.java
+++ src/main/java/mytown/protection/ProtectionManager.java
@@ -250,7 +250,7 @@ public class ProtectionManager {
                             return false;
                         }
                     }
-                }    
+                }
             }
         }
 
